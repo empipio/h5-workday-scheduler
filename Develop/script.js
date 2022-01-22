f333a3757de566317eb6c871a04e55e9b6d95a96
empipio <94937507+empipio@users.noreply.github.com>
2022-01-22 09:34:21 +0000

    comments added to js

--- Develop/script.js
+++ Develop/script.js
@@ -25,19 +25,21 @@
 //add event listener to save buttons
 //clear storage when date changes
 
+//gets todays date from moment and renders to page
 var today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY"));
 
+//used in clearScreen function and for dayPlan variable
 var todaysDate = moment();
 
-//this works now with == rather than ===?
 function colourTimeBlocks() {
+  //get current hour and data index of time block hour
   var currentHour = moment().hour();
   var timeBlockHours = $(".time-block");
 
   for (var i = 0; i < timeBlockHours.length; i++) {
     var currentTimeBlock = $(timeBlockHours[i]).attr("data-index");
-
+    //compare data index of time block to current hour and add class accordingly to change time block colour
     if (currentHour == currentTimeBlock) {
       $(timeBlockHours[i]).addClass("present");
     } else if (currentHour < currentTimeBlock) {
@@ -51,34 +53,37 @@ function colourTimeBlocks() {
 //empty array in which to store saved tasks
 var tasks = [];
 
-//can only save one task
 function saveTask() {
-  var timeBlockHour = $(this).parent().attr("data-index"); //gets data index of parent block to save
+  //gets data index of parent block to save according to which save button clicked
+  var timeBlockHour = $(this).parent().attr("data-index");
+  //traverse DOM to navigate to text area where text has been entered
   var textArea = $(this).parent().children().eq(1);
   var textToSave = textArea.val();
+  //save data index of time block and text in the text area as an object
   var task = { hour: timeBlockHour, plan: textToSave };
 
+  //push task into tasks array
   tasks.push(task);
+  //dayPlan adds in today's date in order to assess when day changes later on
   var dayPlan = {
     date: todaysDate,
     tasks: tasks,
   };
-
+  //saves dayPlan to local storage
   localStorage.setItem("dayPlan", JSON.stringify(dayPlan));
 }
 
-//items not being rendered to screen from LS
 function renderTasks() {
   // go through each task, get each task data and time
   for (var j = 0; j < tasks.length; j++) {
     var task = tasks[j];
     var timeBlockHourElements = $(".time-block");
-
+    //Get data index for each timeblock
     timeBlockHourElements.each(function () {
       var timeBlockHour = $(this).data("index");
-
+      //convert data indices to string in order to compare with task.hour
       var timeBlockHourString = timeBlockHour.toString();
-
+      //if data index and task.hour match then fill that text area with text saved in local storage
       if (timeBlockHourString === task.hour) {
         var fillText = $(this).children().eq(1);
         fillText.html(task.plan);
@@ -87,8 +92,8 @@ function renderTasks() {
   }
 }
 
-//items not being retrieved from local storage
 function init() {
+  //get info from local storage
   var dayPlan = JSON.parse(localStorage.getItem("dayPlan"));
 
   if (dayPlan !== null) {
@@ -98,7 +103,7 @@ function init() {
   renderTasks();
 }
 
-// continually refreshes when preventDefault not included but cannot read when included as undefined?
+//clears local storage and reloads page when date changes
 function clearScreen() {
   var dayPlan = JSON.parse(localStorage.getItem("dayPlan"));
 
@@ -118,5 +123,6 @@ colourTimeBlocks();
 init();
 clearScreen();
 
+//event listener to check for clicks on save buttons, when clicked saveTask function triggered
 var button = $(".saveBtn");
 button.on("click", saveTask);
