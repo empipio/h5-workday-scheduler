08f258be6c531731e3600de9ee6d501ffae7d085
empipio <94937507+empipio@users.noreply.github.com>
2022-02-11 16:08:04 +0000

    code refactored to replace var with const/let

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,17 @@
 //gets todays date from moment and renders to page
-var today = moment();
+const today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY"));
 
 //used in clearScreen function and for dayPlan variable
-var todaysDate = moment();
+const todaysDate = moment();
 
 function colourTimeBlocks() {
   //get current hour and data index of time block hour
-  var currentHour = moment().hour();
-  var timeBlockHours = $(".time-block");
+  let currentHour = moment().hour();
+  const timeBlockHours = $(".time-block");
 
-  for (var i = 0; i < timeBlockHours.length; i++) {
-    var currentTimeBlock = $(timeBlockHours[i]).attr("data-index");
+  for (let i = 0; i < timeBlockHours.length; i++) {
+    const currentTimeBlock = $(timeBlockHours[i]).attr("data-index");
     //compare data index of time block to current hour and add class accordingly to change time block colour
     if (currentHour == currentTimeBlock) {
       $(timeBlockHours[i]).addClass("present");
@@ -24,21 +24,21 @@ function colourTimeBlocks() {
 }
 
 //empty array in which to store saved tasks
-var tasks = [];
+let tasks = [];
 
 function saveTask() {
   //gets data index of parent block to save according to which save button clicked
-  var timeBlockHour = $(this).parent().attr("data-index");
+  const timeBlockHour = $(this).parent().attr("data-index");
   //traverse DOM to navigate to text area where text has been entered
-  var textArea = $(this).parent().children().eq(1);
-  var textToSave = textArea.val();
+  const textArea = $(this).parent().children().eq(1);
+  const textToSave = textArea.val();
   //save data index of time block and text in the text area as an object
-  var task = { hour: timeBlockHour, plan: textToSave };
+  const task = { hour: timeBlockHour, plan: textToSave };
 
   //push task into tasks array
   tasks.push(task);
   //dayPlan adds in today's date in order to assess when day changes later on
-  var dayPlan = {
+  const dayPlan = {
     date: todaysDate,
     tasks: tasks,
   };
@@ -48,17 +48,17 @@ function saveTask() {
 
 function renderTasks() {
   // go through each task, get each task data and time
-  for (var j = 0; j < tasks.length; j++) {
-    var task = tasks[j];
-    var timeBlockHourElements = $(".time-block");
+  for (let j = 0; j < tasks.length; j++) {
+    let task = tasks[j];
+    const timeBlockHourElements = $(".time-block");
     //Get data index for each timeblock
     timeBlockHourElements.each(function () {
-      var timeBlockHour = $(this).data("index");
+      const timeBlockHour = $(this).data("index");
       //convert data indices to string in order to compare with task.hour
-      var timeBlockHourString = timeBlockHour.toString();
+      const timeBlockHourString = timeBlockHour.toString();
       //if data index and task.hour match then fill that text area with text saved in local storage
       if (timeBlockHourString === task.hour) {
-        var fillText = $(this).children().eq(1);
+        let fillText = $(this).children().eq(1);
         fillText.html(task.plan);
       }
     });
@@ -67,7 +67,7 @@ function renderTasks() {
 
 function init() {
   //get info from local storage
-  var dayPlan = JSON.parse(localStorage.getItem("dayPlan"));
+  const dayPlan = JSON.parse(localStorage.getItem("dayPlan"));
 
   if (dayPlan !== null) {
     tasks = dayPlan.tasks;
@@ -78,10 +78,10 @@ function init() {
 
 //clears local storage and reloads page when date changes
 function clearScreen() {
-  var dayPlan = JSON.parse(localStorage.getItem("dayPlan"));
+  const dayPlan = JSON.parse(localStorage.getItem("dayPlan"));
 
   if (dayPlan) {
-    var dayPlanDate = moment(dayPlan.date);
+    const dayPlanDate = moment(dayPlan.date);
 
     if (dayPlanDate.isBefore(todaysDate, "day")) {
       localStorage.clear();
@@ -97,5 +97,5 @@ init();
 clearScreen();
 
 //event listener to check for clicks on save buttons, when clicked saveTask function triggered
-var button = $(".saveBtn");
+const button = $(".saveBtn");
 button.on("click", saveTask);
